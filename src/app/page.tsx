a4c582846ad5f74e4b13f0ee51934e10b0f73279
ThatMovieGuy <131075466+ThatMovieGuyOriginal@users.noreply.github.com>
2025-05-28 15:17:26 -0600

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import FileTreeGenerator from '@/components/FileTreeGenerator'
+
+export default function Home() {
+  return (
+    <div className="py-8">
+      <FileTreeGenerator />
+    </div>
+  )
+}
