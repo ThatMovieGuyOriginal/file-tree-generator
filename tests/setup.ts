c2bc1deb57b2b18c1ed76588b98342dbea6a1d70
ThatMovieGuy <131075466+ThatMovieGuyOriginal@users.noreply.github.com>
2025-05-29 14:40:20 -0600

    Create setup.ts

---
+++ tests/setup.ts
@@ -0,0 +1,11 @@
+// tests/setup.ts
+import { expect, afterEach } from 'vitest'
+import { cleanup } from '@testing-library/react'
+import * as matchers from '@testing-library/jest-dom/matchers'
+
+// extends Vitest's expect
+expect.extend(matchers)
+
+afterEach(() => {
+  cleanup()
+})
