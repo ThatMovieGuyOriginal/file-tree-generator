86a0eb5981b598278ad4a85e9ddcdee59149e1f1
ThatMovieGuy <131075466+ThatMovieGuyOriginal@users.noreply.github.com>
2025-05-30 17:17:30 -0600

    Update and rename src/components/DynamicConfiguration.tsx to src/steps/step-2-configuration/components/DynamicConfiguration.tsx

--- src/steps/step-2-configuration/components/DynamicConfiguration.tsx
+++ src/steps/step-2-configuration/components/DynamicConfiguration.tsx
@@ -1,4 +1,4 @@
-// src/components/DynamicConfiguration.tsx
+// src/steps/step-2-configuration/components/DynamicConfiguration.tsx
 'use client';
 
 import React, { useState, useEffect } from 'react';
